e7c58588a133bb99f968fd00b340d8a03a4527c3
PerlaGar <74082822+PerlaGar@users.noreply.github.com>
2023-02-28 17:22:55 -0600

    Tarea 3 completa (sin error de imagenes)

--- tarea3/rutas.js
+++ tarea3/rutas.js
@@ -2,10 +2,9 @@ const path=require('path');
 const express = require('express');
 
 module.exports=function(app){
-    app.use('/public/css', express.static(path.join(__dirname, '/public/css')))
+    app.use('/public', express.static(path.join(__dirname, 'public')));
 
     app.get('/',function(req,res){
         res.sendFile(path.join(__dirname,'src/vistas/index.html'));
     });   
-    
 }
